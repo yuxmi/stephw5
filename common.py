18b1015f2a91dea0f917b26af203b55bd9ed42ca
yuxmi <105485109+yuxmi@users.noreply.github.com>
2022-06-10 05:53:06 -0400

    Update common.py

--- common.py
+++ common.py
@@ -1,15 +1,22 @@
 def read_input(filename):
+    cities = []
     with open(filename) as f:
-        cities = []
-        for line in f.readlines()[1:]:  # Ignore the first line.
-            xy = line.split(',')
-            cities.append((float(xy[0]), float(xy[1])))
-        return cities
-
+        for data in f.read().splitlines():
+            x = float(data.split(',')[0])
+            y = float(data.split(',')[1])
+            coord = (x,y)
+            cities.append(coord)
+    return cities
 
 def format_tour(tour):
     return 'index\n' + '\n'.join(map(str, tour))
 
-
 def print_tour(tour):
     print(format_tour(tour))
+
+def totalDist(tour, cities):
+    result = 0
+    for i in range(len(tour) - 1):
+        result += distance(cities[tour[i]], cities[tour[i + 1]])
+    result += distance(cities[tour[0]], cities[tour[-1]])
+    return result
